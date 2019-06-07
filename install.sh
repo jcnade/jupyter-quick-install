679fc1b75b8f19b89d6ca46f2d97fea82199a615
jcnade@gmail.com <jcnade@gmail.com>
2019-06-07 13:40:20 +0200

    adding script for jupyter

--- install.sh
+++ install.sh
@@ -3,21 +3,47 @@
 
 # first, some basic update and upgrade
 
-# sudo apt-get update -y
-# sudo apt-get upgrade -y
+ sudo apt-get update -y
+ sudo apt-get upgrade -y
 
 # we need docker
 
+echo "---------------------------------"
 
 sudo apt install apt-transport-https ca-certificates curl software-properties-common
+
+echo "---------------------------------"
+
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+echo "---------------------------------"
+
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+
+echo "---------------------------------"
+
 sudo apt update
+
+echo "---------------------------------"
+
 apt-cache policy docker-ce
-sudo apt install docker-ce
-sudo systemctl status docker
+
+echo "---------------------------------"
+
+sudo apt install docker-ce -y
 
 
+echo "---------------------------------"
+echo "Docker Version"
+docker --version
+echo "---------------"
+
+
+#
+# Download and Run docker run jupyter/scipy-notebook
+#
+
+sudo docker run -d -p 8888:8888 jupyter/scipy-notebook
 
 
 
