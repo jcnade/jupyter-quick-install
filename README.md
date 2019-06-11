638ccacf990c4e12761765f3941e6e3746b1c857
jcnade@gmail.com <jcnade@gmail.com>
2019-06-11 09:41:24 +0200

    more doc

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # jupyter-quick-install
 
-Docker + Jupyter + Ngnix + .htaccess in one command line
+One command line to install Jupyter(dockers) + Ngnix + .htaccess in one command line
+
 
 
 ### Contexte :
@@ -9,24 +10,26 @@ Pour la préparation d'un TP universitaire en machine learning, je souhaite conf
 
 ### Prérequis
 
-* une instance AWS avec un Ubuntu LTS 64bits
-
+* une instance AWS avec un Ubuntu LTS 64bits (t2.medium)
+* un fichier .pem pour se connecter
+* un groupe de sécurité qui ouvre le port SSH, 80
 
 ###  Installation 
 
 
-Step 1 - make the instance
+Step 1 création de l'instance sur AWS
 
-	on AWS -> t2.micro (Ubuntu) 
+	on AWS -> t2.micro (Ubuntu 64bits) 
 
+step 2 - connexion
 
-Step 2
+	ssh -i <your.pem> ubuntu@<public IP>
+
+Step 3 - exécution du script
 
         sudo wget -O - https://raw.githubusercontent.com/jcnade/jupyter-quick-install/master/install.sh | bash
 
-Step 3
-
-
+Step 4 - on doit installer qq module suplémentaires
 
 
 
--- install.sh
+++ install.sh
@@ -8,28 +8,11 @@
 
 # we need docker
 
-echo "---------------------------------"
-
 sudo apt install apt-transport-https ca-certificates curl software-properties-common
-
-echo "---------------------------------"
-
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-
-echo "---------------------------------"
-
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
-
-echo "---------------------------------"
-
 sudo apt update
-
-echo "---------------------------------"
-
 apt-cache policy docker-ce
-
-echo "---------------------------------"
-
 sudo apt install docker-ce -y
 
 
@@ -39,11 +22,77 @@ docker --version
 echo "---------------"
 
 
+
+#-------------------------------------------------------------
+# we need to add extra missing module, to this basic docker.
+#-------------------------------------------------------------
+
+sudo docker exec -it 52f73950b56c bash
+sudo docker exec -it 1309d95458c2 bash
+
+pip install pyspark
+pip install findspark
+pop install helpers
+conda install pyspark
+conda install SparkSession
+
+
+
 #
 # Download and Run docker run jupyter/scipy-notebook
 #
 
 sudo docker run -d -p 8888:8888 jupyter/scipy-notebook
+
+f0e488eae184d1b11f1c74251e799f2866bb7fea7f40cc77cc49e629af7b2ef6
+52f73950b56ca669a15feb3f51c365f3c87a21c566b04f7a070b715ebbc22c8e
+
+sudo docker ps -a
+
+# I need the docket contener ID for my sript
+ID=$(sudo docker ps -a -q)
+
+
+#-------------------------------------
+# grabbing the jupyther secret token
+#-------------------------------------
+
+sudo docker ps -a
+
+sudo docker exec $ID jupyter notebook list
+
+
+
+# docker stop cf66ff4874b9
+# docker start cf66ff4874b9
+
+
+# http://54.171.92.149:8888/?token=99189e27e3f4a683d64fbf682efb3b6b14096db24c328d8d :: /home/jovyan
+
+#
+# Fire Wall Config (with ufw)
+#
+
+sudo ufw status
+sudo ufw default allow
+sudo ufw allow ssh
+sudo ufw allow www
+sudo ufw allow 8888
+sudo ufw allow 80
+sudo ufw allow ssh
+sudo ufw allow 'Nginx HTTP'
+sudo ufw status
+sudo ufw enable
+
+#
+# ngnix
+#
+
+sudo apt-get install nginx -y
+
+ip addr show eth0 | grep inet | awk '{ print $2; }' | sed 's/\/.*$//'
+
+
 
 
 
