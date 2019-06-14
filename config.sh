f6bab5199d74c73b578dff07e55f7e9536d0abe6
jcnade@gmail.com <jcnade@gmail.com>
2019-06-14 14:01:19 +0200

    tuning command

---
+++ config.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+#
+#
+# Config Update, inside the docker
+#
--- install.sh
+++ install.sh
@@ -98,7 +98,7 @@ echo ""
 
 niceTitle "Docker"
 
-sudo apt install apt-transport-https ca-certificates curl software-properties-common
+sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
 sudo apt update
